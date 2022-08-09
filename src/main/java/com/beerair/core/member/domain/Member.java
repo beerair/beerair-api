f1756e4bd403a96cbd92599f2f2c5e5de2c65489
darkant99 <darkant99@naver.com>
2022-08-09 22:29:18 +0900

    test: 소셜 사용자 첫 로그인 후 사용자 정보 등록

--- src/main/java/com/beerair/core/member/domain/Member.java
+++ src/main/java/com/beerair/core/member/domain/Member.java
@@ -13,6 +13,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.Comment;
+
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,12 +23,16 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Comment("이메일")
     private String email;
 
+    @Comment("닉네임")
     private String nickname;
 
+    @Comment("핸드폰 번호")
     private String phoneNumber;
 
+    @Comment("프로필 사진")
     private String profileUrl;
 
     private Role role;
--- src/test/java/com/beerair/core/acceptance/member/MemberStepDefs.java
+++ src/test/java/com/beerair/core/acceptance/member/MemberStepDefs.java
@@ -3,6 +3,8 @@ package com.beerair.core.acceptance.member;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.ScenarioScope;
 
 @ScenarioScope
@@ -10,8 +12,15 @@ public class MemberStepDefs {
     @Autowired
     private MemberStepClient memberHttpClient;
 
-    @Given("{string} 이메일로 회원가입 했을때")
-    public void sign(String email) {
-        memberHttpClient.sign();
+    @Given("{string} 소셜 계정으로 로그인을 요청하면")
+    public void login(String testUserSpec) {
+    }
+
+    @When("사용자 정보 등록을 요청 하면")
+    public void sign(String testUsername) {
+    }
+
+    @Then("반환된 권한에 {string} 권한이 포함된다.")
+    public void containsRole() {
     }
 }
