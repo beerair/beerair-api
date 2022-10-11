2361e33eec2f57c7fe7cfe9a2c4477d24395ef81
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-10-11 22:13:50 +0900

    :bug: config 수정

--- src/main/java/com/beerair/core/config/cruiser/CruiserConfig.java
+++ src/main/java/com/beerair/core/config/cruiser/CruiserConfig.java
@@ -4,7 +4,6 @@ import com.beerair.core.cruiser.domain.CruiserClient;
 import com.beerair.core.cruiser.domain.SlackCruiser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,7 +16,6 @@ public class CruiserConfig {
     private final WebClient.Builder webClient;
 
     @Bean
-    @ConditionalOnProperty(prefix = "monitor.slack", name = "cruiser")
     public CruiserClient slackCruiser() {
         return new SlackCruiser(cruiserProperties, webClient);
     }
