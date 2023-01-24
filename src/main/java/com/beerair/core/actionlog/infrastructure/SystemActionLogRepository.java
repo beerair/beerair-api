2b9e0819a5cd4492a78d1c9d6c19f532ff495628
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-01-24 14:19:56 +0900

    fix: 잘못된 어노테이션 표기 변경

--- src/main/java/com/beerair/core/actionlog/infrastructure/SystemActionLogRepository.java
+++ src/main/java/com/beerair/core/actionlog/infrastructure/SystemActionLogRepository.java
@@ -2,12 +2,13 @@ package com.beerair.core.actionlog.infrastructure;
 
 import com.beerair.core.actionlog.domain.SystemActionLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
-@Service
+@Repository
 public interface SystemActionLogRepository extends JpaRepository<SystemActionLog, Long> {
     @Transactional(readOnly = true)
     Long countAllByCreatedAtBetween(LocalDateTime startAt, LocalDateTime endAt);
